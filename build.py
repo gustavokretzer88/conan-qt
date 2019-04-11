da644261a805a94eeed0562ca10cf44d2120bfc2
ericLemanissier <eric.lemanissier@gmail.com>
2019-04-11 11:55:29 +0200

    added azure android CI [skip travis] [skip appveyor]

--- build.py
+++ build.py
@@ -2,8 +2,16 @@
 # -*- coding: utf-8 -*-
 
 from bincrafters import build_template_default
+import os
 
 if __name__ == "__main__":
 
     builder = build_template_default.get_builder(build_policy="missing")
+    if os.getenv("CONAN_DOCKER_IMAGE", "").startswith("conanio/android-clang8"):
+        for it in builder.items:
+            it.settings["os"] = "Android"
+            it.settings["arch"] = os.getenv("CONAN_ARCHS")
+            it.settings["compiler"] = "clang"
+            it.settings["compiler.version"] = "8"
+            it.settings["compiler.libcxx"] = "libc++"
     builder.run()
